1f0d5dc99c09837b84de30c4f55c5c027982688c
Andrés <andresvrif@gmail.com>
2021-07-02 15:08:10 -0500

    Create users and roles for login

---
+++ import.sql
@@ -0,0 +1,8 @@
+/* Creamos algunos usuarios */
+INSERT INTO users (username, password, enabled) VALUES('andres', '$2a$10$debpqzhpXFd4O/Lx3kAhX.KeOqhesTfrJMStixsYSqcQFvIXicHbC', 1); --password 12345
+INSERT INTO users (username, password, enabled) VALUES('admin', '$2a$10$X.1R6428OlgxcWCEpX2SSuFKMIBPpQ/EFG/e3GTZm57BWvzZEzdAO', 1);  --password 12345
+
+/* Creamos algunos roles para los usuarios */
+INSERT INTO authorities (user_id, authority) VALUES(1,'ROLE_USER');
+INSERT INTO authorities (user_id, authority) VALUES(2,'ROLE_USER');
+INSERT INTO authorities (user_id, authority) VALUES(2,'ROLE_ADMIN');
